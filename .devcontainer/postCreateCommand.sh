06d1a8b0db3558bf4e68138074afcb3e331e96b0
Ron Regan <rreganjr@acm.org>
2023-11-18 12:58:33 -0500

    Delete .devcontainer directory
    changes are blowing up the container, I want to create a new one

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-# post setup
-sudo su -
-cd /opt
-wget https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/3.1.5/spring-boot-cli-3.1.5-bin.tar.gz
-gunzip spring-boot-cli-3.1.5-bin.tar.gz
-tar -xvf spring-boot-cli-3.1.5-bin.tar
