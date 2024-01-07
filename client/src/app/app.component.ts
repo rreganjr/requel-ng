f44c7189f0669f4a4e0be633f15ad932c8488195
ron <rreganjr@acm.org>
2024-01-07 01:18:13 +0000

    I borrowed contenteditable-value-accessor.ts from https://github.com/Tinkoff/angular-contenteditable-accessor, I tried to fork and update angular, but it failed. I changed it to a standalone directive and use it in app.component.html on the project name and vision like [(ngModel)]="project.name"

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TeamStore } from './team/team-store.service';
@@ -9,21 +9,26 @@ import { GoalListComponent } from './components/goal-list/goal-list.component';
 import { TeamTableComponent } from './components/team-table/team-table.component';
 import {makeGroupId} from './id-builder';
 import { FeatureListComponent } from './components/feature-list/feature-list.component';
+import { FormsModule } from '@angular/forms';
+import { ContenteditableValueAccessor } from './util/contenteditable-value-accessor';
 
 @Component({
   selector: 're-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, GoalListComponent, TeamTableComponent, FeatureListComponent], 
+  imports: [CommonModule, RouterOutlet, GoalListComponent, TeamTableComponent, FeatureListComponent, FormsModule, ContenteditableValueAccessor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ProjectStore, TeamStore],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   projectId: string = "project-1"
   project: Project
 
   public makeGroupId = makeGroupId
   constructor(private projectStore: ProjectStore, private teamStore: TeamStore, private goalStore: GoalStore) {
-    this.project = projectStore.getProject(this.projectId)
+    this.project = this.projectStore.getProject(this.projectId);
+  }
+
+  public ngOnInit(): void {
   }
 }
